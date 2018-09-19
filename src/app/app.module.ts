0ac2c14cc0b9733c00a882d4a0fe3b4b1ca18c25
yurigoular <yurigoular@gmail.com>
2018-09-18 21:15:57 -0300

    init login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule, Button } from 'primeng/button';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RouterModule, Routes } from '../../node_modules/@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent},
+  { path: '',      component: AppComponent},
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    InputTextModule,
+    ButtonModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
